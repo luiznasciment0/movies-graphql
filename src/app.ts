5f4cba986cee05a2bde6fd11b7cc3e1cd0cfe105
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-09 00:34:11 -0300

    simplify server initialization

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,17 @@
 import Koa from 'koa'
-import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
-import convert from 'koa-convert'
+import mount from 'koa-mount'
 import graphqlHTTP from 'koa-graphql'
 
 import schema from './schema/schema'
 
 const app = new Koa()
-const router = new Router()
 
 app.use(bodyParser())
 
-router.get('/', ctx => {
-  const info = [
-    '/graphql - GraphiQL',
-    '/status - Status server'
-  ]
-
-  ctx.status = 200
-  ctx.body = info.join('\n')
-})
-
-router.get('/status', ctx => {
-  console.log('ta aqui')
-  ctx.status = 200
-  ctx.body = "running"
-})
-
-const graphQLApp = convert(
-  graphqlHTTP(async (_request, response, ctx) => {
-    return {
-      graphiql: true,
-      schema,
-      rootValue: {
-        request: response.req,
-      },
-      context: {
-        ctx,
-      }
-    }
-  })
-)
-
-router.all('/graphql', graphQLApp)
-app.use(router.routes())
+app.use(mount('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema,
+})))
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import { createServer } from 'http'
 import app from './app'
 import { config } from './config'
 
-(async () => {
-  const server = createServer(app.callback())
-
-  server.listen(config.PORT, () => {
-    console.log(`server running at http://localhost:${config.PORT}`)
-  })
-})()
+app.listen(config.PORT, () => {
+  console.log(`server running at http://localhost:${config.PORT}`)
+})
