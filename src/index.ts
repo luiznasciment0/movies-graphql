0381da4fea491f5c4d63db37bafa0feafda22ae1
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-09 03:56:10 -0300

    heroku

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import app from './app'
 import { config } from './config'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 app.listen(config.PORT, () => {
   console.log(`server running at http://localhost:${config.PORT}`)
