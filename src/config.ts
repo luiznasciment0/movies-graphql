80814f34da778f6b3908def8d78398912c6ebcd1
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-09 01:48:14 -0300

    create youtube api key

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ dotenv.config()
 
 export const config = {
   BASE_URL: process.env.BASE_URL,
+  YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
   API_KEY: process.env.API_KEY,
   API_HOST: process.env.API_HOST,
   PORT: process.env.PORT || 9000,
